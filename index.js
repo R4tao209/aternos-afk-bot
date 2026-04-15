b52981b4780998a82fe09619b8f008fdfe0c91ad
R4tao209 <reinsta2009@gmail.com>
2026-04-15 20:49:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
-  host: 'YOUR_SERVER_IP', // without port (ex: play.aternos.me)
-  port: 25565, // Default Minecraft port
-  username: 'AFK_Bot' // Bot username
+  host: 'Resenha1522.aternos.me', // without port (ex: play.aternos.me)
+  port: 40989, // Default Minecraft port
+  username: 'BOTFDP' // Bot username
 });
 
 bot.on('spawn', () => {
